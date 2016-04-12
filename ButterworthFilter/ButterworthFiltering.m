e5dd7ab22223238538276bd07ed89b189ede5ec7
Pedro Pena <p.pena3@umiami.edu>
2016-04-12 10:05:46 -0400

    Added new graphs

--- ButterworthFilter/ButterworthFiltering.m
+++ ButterworthFilter/ButterworthFiltering.m
@@ -20,7 +20,58 @@ for i=1:R
   [S, P] = KalmanFilter(S, P, X.EEG2(i)); 
 end
 
+figure;
+subplot(3, 2, 1);
 plot(X.EEG2); hold on;
 plot(RS); hold on;
 plot(filteredData); 
 xlim([0 2000]);
+title('Butterworth/Kalman Filter');
+
+
+%% Plot Data from EEGLab
+subplot(3, 2, 2);
+plot(RawSignal);
+title('Raw Data');
+
+subplot(3, 2, 3);
+plot(CLEANSignal);
+title('Clean Data');
+%% Filter Raw Data Using Kalman Filter
+[R, C] = size(RawSignal);
+RS2 = zeros(R, C);
+
+ for i=1:C %Works best with Q = 0.000001; R = 1;
+     S = 5;
+     P = 1;
+    for j=1:R
+          RS2(j, i) = S;
+          [S, P] = KalmanFilter(S, P, RawSignal(j, i)); 
+    end
+ end
+
+subplot(3, 2, 4);
+plot(RS2);
+title('Kalman Filtered Data')
+%plot(RawSignal(:,1));
+
+%% Filter Raw Data Using Particle Filter
+C = 4;
+RS3 = zeros(R, C);
+ M = 25;
+ for i=1:C 
+    % initialize particles
+    V = 100;
+    X_p = ones(M, 1);
+    for m = 1:M
+        X_p(m) = sqrt(V) * randn;
+    end
+    for j=1:R
+          RS3(j, i) = mean(X_p);
+          X_p = ParticleFilter(X_p, RawSignal(j, i)); 
+    end
+ end
+ 
+subplot(3, 2, 5);
+plot(RS3(:, 1));
+title('Particle Filtered Data')
